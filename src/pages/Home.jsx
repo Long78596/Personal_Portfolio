5f3d6929cd1e295322bfcf8e582e45cc5b6165fd
Thanh Long <longkolp16@gmail.com>
2025-05-18 15:20:19 +0700

    chức năng chuyển đổi giao diện sáng/tối (dark mode toggle)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { ThemeToggle } from "../Component/ThemeToggle";
+
+export const Home=()=>{
+     return (
+       <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+         {/* Theme Toggle */}
+          <ThemeToggle />
+         {/* Background Efffects */}
+
+         {/* Navbar */}
+         {/* Main content */}
+
+         {/* Footer */}
+       </div>
+     );
+}
