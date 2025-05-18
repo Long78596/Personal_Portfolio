0bcc8fefc0f6305b4c8c04f64800626d49960bde
Thanh Long <longkolp16@gmail.com>
2025-05-18 15:58:18 +0700

    xong chức năng nền ngôi sao , hiệu ứng sao băng

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { ThemeToggle } from "../Component/ThemeToggle";
-
+import {StarBackground} from "@/Component/StarBackground"
 export const Home=()=>{
      return (
        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
          {/* Theme Toggle */}
           <ThemeToggle />
          {/* Background Efffects */}
-
+          <StarBackground />
          {/* Navbar */}
          {/* Main content */}
 
