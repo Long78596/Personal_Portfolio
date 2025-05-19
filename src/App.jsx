70a2622461186b09eb0c5022262c9bdc1e07cf68
Thanh Long <longkolp16@gmail.com>
2025-05-19 10:59:12 +0700

    xong chức năng contactsection, footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-
+import {Toaster} from "@/components/ui/toaster"
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="*" element={<NotFound />} /> 
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
